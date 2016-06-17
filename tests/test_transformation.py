eb98ee2c1b8310640f67c4ad1bb4580fa8afd055
Paulina1 <paulina.malgorzata.lach@cern.cech>
2016-06-17 09:50:49 +0200

    global: new schemas transforation function
    * NEW Adds a function which removes references from
      JSON schemas.
    
    Signed-off-by: PaulinaLach <paulina.malgorzata.lach@cern.ch>

---
+++ tests/test_transformation.py
@@ -0,0 +1,297 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of DoSchema.
+# Copyright (C) 2016 CERN.
+#
+# DoSchema is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# DoSchema is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DoSchema; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+import os
+import unittest
+
+import jsonschema
+import pytest
+
+from doschema.transform import Transform
+
+
+def test_ref_no_array_pass():
+    v1 = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        },
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {"$ref": "#/definitions/address"},
+            "shipping_address": {"$ref": "#/definitions/address"}
+        }
+    }
+    exp = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        },
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            },
+            "shipping_address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        }
+    }
+    obj = Transform(v1)
+    result = obj.transform()
+    print(result)
+    assert exp == result
+
+
+def test_ref_no_conflict_inside_schema_pass():
+    """Test that having no conflict in schema with references passes."""
+    v1 = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        },
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {"$ref": "#/definitions/address"},
+            "shipping_address": {
+                "allOf": [
+                    {"$ref": "#/definitions/address"},
+                    {
+                        "properties":   {
+                            "type": {"enum": ["residential", "business"]}
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    exp = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        },
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            },
+            "shipping_address": {
+                "allOf": [
+                    {
+                        "type": "object",
+                        "properties": {
+                            "street_address": {"type": "string"}
+                        }
+                    },
+                    {
+                        "properties": {
+                            "type": {"enum": ["residential", "business"]}
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    obj = Transform(v1)
+    result = obj.transform()
+    assert exp == result
+
+
+def test_ref_conflict_inside_schema():
+    """Test that having an invalid reference in the schema raises
+    RefResolutionError."""
+    v1 = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {"$ref": "#/definitions/differentaddress"},
+            "shipping_address": {
+                "allOf": [
+                    {"$ref": "#/definitions/address"},
+                    {
+                        "properties":   {
+                            "type": {"enum": ["residential", "business"]}
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    obj = Transform(v1)
+    with pytest.raises(jsonschema.exceptions.RefResolutionError):
+        obj.transform()
+
+
+def test_ref_conflict_between_schemas():
+    """Test that having a conflict between schemas with references passes."""
+    v1 = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        },
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {
+                "$ref": "test_outside.json#/definitions/address"
+
+            },
+            "shipping_address": {
+                "allOf": [
+                    {"$ref": "#/definitions/address"},
+                    {
+                        "properties":   {
+                            "type": {"enum": ["residential", "business"]}
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    exp = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "string"}
+                }
+            }
+        },
+
+        "type": "object",
+
+        "properties": {
+            "billing_address": {
+                "type": "object",
+                "properties": {
+                    "street_address": {"type": "differenttype"}
+                }
+            },
+            "shipping_address": {
+                "allOf": [
+                    {
+                        "type": "object",
+                        "properties": {
+                            "street_address": {"type": "string"}
+                        }
+                    },
+                    {
+                        "properties":   {
+                            "type": {"enum": ["residential", "business"]}
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    obj = Transform(v1, 'file:///' + os.getcwd() + '/')
+    result = obj.transform()
+    print(result)
+    assert exp == result
+
+
+def test_pattern():
+    v1 = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "^[a-zA-Z0-9]+$": {"type": "string"}
+        },
+
+        "type": "object",
+
+        "properties": {
+            "^[a-zA-Z]+$": {"$ref": "#/definitions"}
+        }
+    }
+    exp = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+
+        "definitions": {
+            "^[a-zA-Z0-9]+$": {"type": "string"}
+        },
+
+        "type": "object",
+
+        "properties": {
+            "^[a-zA-Z]+$": {
+                "^[a-zA-Z0-9]+$": {"type": "string"}
+            }
+        }
+    }
+    obj = Transform(v1)
+    result = obj.transform()
+    assert exp == result
